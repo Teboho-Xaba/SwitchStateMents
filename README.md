19c7111aafdc3fc25e6fe97ddb57a574e3e73e15
Tebza <99969842+Teboho-Xaba@users.noreply.github.com>
2023-12-12 21:40:09 +0200

    Incomplete with a glimmer of progress

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # SwitchStateMents
- 
+
+A practice exercise of logic and the use of "switch" statements instead of "IF" statements
---
+++ main.js
@@ -0,0 +1,12 @@
+const begin = confirm("Do you want to play Rock, paper, scissors?")
+
+switch (playerOne){
+    case "rock":
+        return 'win';
+    break;
+    case "paper":
+        return 'tie'
+    break;
+    default:
+        return 'computer wins';
+}
